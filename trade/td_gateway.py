fc945345795cfd5a52dd5a0f230411b3e7b54ea1
bo.song <bo.song@gotogameday.com>
2025-06-18 14:46:45 +0800

    fix

--- trade/td_gateway.py
+++ trade/td_gateway.py
@@ -7,7 +7,8 @@ from decimal import Decimal
 from BoQuantitativeSystem.config.config import Configs
 from BoQuantitativeSystem.trade.bian_future_api import BiFutureTd
 from BoQuantitativeSystem.utils.dingding import Dingding
-from BoQuantitativeSystem.utils.exchange_enum import ExchangeType
+from BoQuantitativeSystem.utils.exchange_enum import ExchangeType, OffsetFlag, Direction, OrderPriceType
+from BoQuantitativeSystem.utils.sys_exception import common_exception
 
 
 class TDGateway:
@@ -25,6 +26,71 @@ class TDGateway:
             self.client.connect()
             self.account_book = self.client.account_book
 
+    @common_exception(log_flag=True)
+    def on_query_account(self, data):
+        pass
+
+    def cancel_cancel_all_order(self, instrument):
+        self.client.cancel_all_order(instrument)
+
+    @common_exception(log_flag=True)
+    def insert_order(self, instrument: str, offset_flag: OffsetFlag, direction: Direction,
+                     order_price_type: OrderPriceType, price: float, volume: float,
+                     cancel_delay_seconds: int = 0, **kwargs) -> str:
+        """ 向交易所报单
+        做多开仓=OPEN:LONG    做空开仓=OPEN:SHORT
+        做多平仓=CLOSE:LONG   做空平仓=CLOSE:SHORT
+        """
+        instrument_book = self.account_book.get_instrument_book(instrument + f'.{self.exchange_type}')
+        min_volume_muti = int(1 / float(instrument_book.min_volume_step))
+        min_price_step = instrument_book.min_price_step
+
+        order_step_muti = 10
+        for param in Configs.strategy_list:
+            if instrument == param.get('instrument'):
+                order_step_muti = param['order_step_muti']
+
+        if offset_flag == OffsetFlag.OPEN:
+            if direction == Direction.LONG:
+                trade_price = Decimal(price) + Decimal(min_price_step) * order_step_muti
+            elif direction == Direction.SHORT:
+                trade_price = Decimal(price) - Decimal(min_price_step) * order_step_muti
+        elif offset_flag == OffsetFlag.CLOSE:
+            if direction == Direction.LONG:
+                trade_price = Decimal(price) - Decimal(min_price_step) * order_step_muti
+            elif direction == Direction.SHORT:
+                trade_price = Decimal(price) + Decimal(min_price_step) * order_step_muti
+
+        if 'cash' in kwargs:
+            volume = kwargs['cash'] / float(price)
+            volume = round(volume * min_volume_muti) / min_volume_muti
+            self.logger.info(f'cal vol: min_volume_muti={min_volume_muti} cash={kwargs["cash"]} '
+                             f'price={trade_price} volume={volume} order_step_muti={order_step_muti}')
+
+        req = {
+            'instrument': instrument,
+            'offset_flag': offset_flag,
+            'direction': direction,
+            'order_price_type': order_price_type,
+            'price': str(trade_price),
+            'volume': str(volume),
+            'cancel_delay_seconds': cancel_delay_seconds,
+        }
+        self.logger.info(f'<insert_order> instrument={instrument} offset_flag={offset_flag} direction={direction} '
+                         f'order_price_type={order_price_type} trade_price={trade_price} volume={volume}')
+
+        client_order_id = self.client.insert_order(
+            **req, **kwargs)
+
+        return client_order_id
+
+    @common_exception(log_flag=True)
+    def on_order(self, rtn_order):
+        pass
+
+    @common_exception(log_flag=True)
+    def on_trade(self, rtn_trade):
+        pass
 
     def get_api_configs(self):
         return Configs.crypto_setting
