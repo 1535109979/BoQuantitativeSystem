1c8b48a8a8006a9faa5f6ce5c3b43ebafebc01aa
bo.song <bo.song@gotogameday.com>
2025-08-01 14:03:13 +0800

    fix

--- database/read_db.py
+++ database/read_db.py
@@ -8,11 +8,13 @@ with sqlite3.connect(Configs.root_fp + 'database/' + 'bian_f_data.db') as conn:
     df = pd.read_sql('select * from account_value', conn)
     print(df)
     df = pd.read_sql('select * from trade_info', conn)
-    result = df.groupby('instrument')[['profit', 'commission']].sum().reset_index()
+    sum_df = df.groupby('instrument')[['profit', 'commission']].sum().reset_index()
+    count_df = df[df['offset'] == 'close'].groupby('instrument').size().reset_index(name='close_count')
+    result = pd.merge(sum_df, count_df, on='instrument', how='left').fillna({'close_count': 0})
     print(df)
     print(result)
-    df = pd.read_sql('select * from order_info', conn)
-    print(df)
+    # df = pd.read_sql('select * from order_info', conn)
+    # print(df)
 
 
 
