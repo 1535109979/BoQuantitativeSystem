2ae868e0406f728afa5ad698b6d24a26c7ae0f3d
bo.song <bo.song@gotogameday.com>
2025-08-01 13:57:50 +0800

    fix

--- database/read_db.py
+++ database/read_db.py
@@ -7,9 +7,9 @@ from BoQuantitativeSystem.config.config import Configs
 with sqlite3.connect(Configs.root_fp + 'database/' + 'bian_f_data.db') as conn:
     df = pd.read_sql('select * from account_value', conn)
     print(df)
-    df = pd.read_sql('select * from TradeInfo', conn)
+    df = pd.read_sql('select * from trade_info', conn)
     print(df)
-    df = pd.read_sql('select * from OrderInfo', conn)
+    df = pd.read_sql('select * from order_info', conn)
     print(df)
 
 
